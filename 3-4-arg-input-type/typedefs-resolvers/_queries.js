0b302387f222a396ee67f723876f1f91a85d94d8
jungmin Lee <jungminlee@jungminui-MacBookPro.local>
2021-09-02 11:00:51 +0900

    3-4 fix graphql

--- 3-4-arg-input-type/typedefs-resolvers/_queries.js
+++ 3-4-arg-input-type/typedefs-resolvers/_queries.js
@@ -3,6 +3,20 @@ const { gql } = require('apollo-server')
 const typeDefs = gql`
     type Query {
         people: [People],
+        peopleFiltered(
+            team: Int, 
+            sex: Sex, 
+            blood_type: BloodType, 
+            from: String
+        ): [People]
+        peoplePaginated(
+            team: Int, 
+            sex: Sex, 
+            blood_type: BloodType, 
+            from: String,
+            page: Int!,
+            per_page: Int!
+        ): [People]
         equipments: [Equipment]
         equipmentAdvs: [EquipmentAdv]
         softwares: [Software]
